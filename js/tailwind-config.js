1f4717e164b75c42f24b6a3e8d688b85532d23c4
looknowvn <looknow.vn@gmail.com>
2025-11-14 10:13:58 +0700

    Add files via upload

---
+++ js/tailwind-config.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#D4AF37",
+                "background-light": "#FFFFFF",
+                "background-dark": "#161d30",
+                "text-light": "#FFFFFF",
+                "text-dark": "#FCDD8A",
+                "btn-light": "#161D30",
+                "subtle-light": "#F8F8F8",
+                "subtle-dark": "#1f2937",
+                "border-light": "#EAEAEA",
+                "border-dark": "#30374A",
+            },
+            fontFamily: {
+                "display": ["Open Sans", "sans-serif"]
+            },
+            borderRadius: {
+                "DEFAULT": "1rem",
+                "lg": "2rem",
+                "xl": "3rem",
+                "full": "9999px"
+            },
+        },
+    },
+}
